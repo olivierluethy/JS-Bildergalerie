39265a9bd496870f2241381498ad096c1f2adb6a
Oli7000 <olivier.luethy@gmx.net>
2022-01-28 08:16:41 +0100

    Adding local storage and some css

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/* Local Storage Key */
-let key = 0;
-
-window.onload = function exampleFunction() {
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        alert(`${key}: ${localStorage.getItem(key)}`);
-    }
-}
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -34,15 +24,90 @@ window.onclick = function(event) {
     }
 }
 
-/* Add Image Function */
-function addImage() {
-    var img = document.createElement("img");
-    img.src = document.getElementById("imgURL").value;
-    console.log(img.href);
-    var container = document.getElementById("imageGallery");
-    container.appendChild(img);
-    modal.style.display = "none";
-    localStorage.setItem(key, img.src)
-    alert(localStorage.getItem(key))
-    key++;
+
+window.onload = init;
+
+function init() {
+    var button = document.getElementById('mehr');
+    button.onclick = ToDoHinzufügen;
+    var clearButton = document.getElementById('loeschen');
+    clearButton.onclick = allesLöschen;
+    var imageGalleryArray = HolEinträge();
+    for (var i = 0; i < imageGalleryArray.length; i++) {
+        var aufgabeNr = imageGalleryArray[i];
+        var value = JSON.parse(localStorage[aufgabeNr]);
+        insDOMschreiben(aufgabeNr, value);
+    }
+}
+
+function HolEinträge() {
+    var imageGalleryArray = localStorage.getItem('imageGalleryArray');
+    if (!imageGalleryArray) {
+        imageGalleryArray = [];
+        localStorage.setItem('imageGalleryArray', JSON.stringify(imageGalleryArray));
+    } else {
+        imageGalleryArray = JSON.parse(imageGalleryArray);
+    }
+    return imageGalleryArray;
+}
+
+function ToDoHinzufügen() {
+    var imageGalleryArray = HolEinträge();
+    var value = document.getElementById('eingabe')
+        .value;
+    if (value != '') {
+        var currentDate = new Date();
+        var aufgabeNr = 'aufgabe_' + currentDate.getTime()
+        var aufgabeText = {
+            'value': value
+        };
+        localStorage.setItem(aufgabeNr, JSON.stringify(aufgabeText));
+        imageGalleryArray.push(aufgabeNr);
+        localStorage.setItem('imageGalleryArray', JSON.stringify(imageGalleryArray));
+        insDOMschreiben(aufgabeNr, aufgabeText);
+        document.getElementById('eingabe')
+            .value = ' ';
+    } else {
+        alert('Bitte geben Sie ihre URL ein!');
+    }
+}
+
+function toDoLöschen(e) {
+    var aufgabeNr = e.target.id;
+    var imageGalleryArray = HolEinträge();
+    if (imageGalleryArray) {
+        for (var i = 0; i < imageGalleryArray.length; i++) {
+            if (aufgabeNr == imageGalleryArray[i]) {
+                imageGalleryArray.splice(i, 1);
+            }
+        }
+        localStorage.removeItem(aufgabeNr);
+        localStorage.setItem('imageGalleryArray', JSON.stringify(imageGalleryArray));
+        ausDOMentfernen(aufgabeNr);
+    }
+}
+
+function insDOMschreiben(aufgabeNr, ItemObj) {
+    var imageGallery = document.getElementById('imageGallery');
+    var eintrag = document.createElement('img');
+    eintrag.setAttribute('id', aufgabeNr);
+    eintrag.src = ItemObj.value;
+    eintrag.title = "Löschen";
+    imageGallery.appendChild(eintrag);
+    eintrag.onclick = toDoLöschen;
+}
+
+function ausDOMentfernen(aufgabeNr) {
+    var eintrag = document.getElementById(aufgabeNr);
+    eintrag.parentNode.removeChild(eintrag);
+}
+
+function allesLöschen() {
+    localStorage.clear();
+    var ItemList = document.getElementById('imageGallery');
+    var imageGallery = ItemList.childNodes;
+    for (var i = imageGallery.length - 1; i >= 0; i--) {
+        ItemList.removeChild(imageGallery[i]);
+    }
+    var imageGalleryArray = HolEinträge();
 }
