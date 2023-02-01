5a2e8efb3b6c3da2df4aba7faadde2530bbe8abe
Olivier_Luethy <olivier@kauz.ch>
2023-02-01 09:38:17 +0100

    js localstorage change and styling

--- script.js
+++ script.js
@@ -1,113 +1,86 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// Variables
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
+// Modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
 
-
-window.onload = init;
+// ToDo
+document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    var button = document.getElementById('mehr');
-    button.onclick = ToDoHinzufügen;
-    var clearButton = document.getElementById('loeschen');
-    clearButton.onclick = allesLöschen;
-    var imageGalleryArray = HolEinträge();
-    for (var i = 0; i < imageGalleryArray.length; i++) {
-        var aufgabeNr = imageGalleryArray[i];
-        var value = JSON.parse(localStorage[aufgabeNr]);
-        insDOMschreiben(aufgabeNr, value);
+    const button = document.getElementById('mehr');
+    button.addEventListener('click', addToDo);
+    const clearButton = document.getElementById('loeschen');
+    clearButton.addEventListener('click', clearAll);
+    const toDos = getToDos();
+    for (const toDo of toDos) {
+        const data = JSON.parse(localStorage.getItem(toDo));
+        insertToDOM(toDo, data);
     }
 }
 
-function HolEinträge() {
-    var imageGalleryArray = localStorage.getItem('imageGalleryArray');
-    if (!imageGalleryArray) {
-        imageGalleryArray = [];
-        localStorage.setItem('imageGalleryArray', JSON.stringify(imageGalleryArray));
-    } else {
-        imageGalleryArray = JSON.parse(imageGalleryArray);
+function getToDos() {
+    let toDos = JSON.parse(localStorage.getItem('toDos'));
+    if (!toDos) {
+        toDos = [];
+        localStorage.setItem('toDos', JSON.stringify(toDos));
     }
-    return imageGalleryArray;
+    return toDos;
 }
 
-function ToDoHinzufügen() {
-    var imageGalleryArray = HolEinträge();
-    var value = document.getElementById('eingabe')
-        .value;
-    if (value != '') {
-        var currentDate = new Date();
-        var aufgabeNr = 'aufgabe_' + currentDate.getTime()
-        var aufgabeText = {
-            'value': value
+function addToDo() {
+    const input = document.getElementById('eingabe').value;
+    if (input !== '') {
+        const date = new Date();
+        const id = `toDo_${date.getTime()}`;
+        const toDo = {
+            'value': input
         };
-        localStorage.setItem(aufgabeNr, JSON.stringify(aufgabeText));
-        imageGalleryArray.push(aufgabeNr);
-        localStorage.setItem('imageGalleryArray', JSON.stringify(imageGalleryArray));
-        insDOMschreiben(aufgabeNr, aufgabeText);
-        document.getElementById('eingabe')
-            .value = ' ';
+        localStorage.setItem(id, JSON.stringify(toDo));
+        const toDos = getToDos();
+        toDos.push(id);
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+        insertToDOM(id, toDo);
+        document.getElementById('eingabe').value = '';
     } else {
-        alert('Bitte geben Sie ihre URL ein!');
+        alert('Please enter a URL!');
     }
 }
 
-function toDoLöschen(e) {
-    var aufgabeNr = e.target.id;
-    var imageGalleryArray = HolEinträge();
-    if (imageGalleryArray) {
-        for (var i = 0; i < imageGalleryArray.length; i++) {
-            if (aufgabeNr == imageGalleryArray[i]) {
-                imageGalleryArray.splice(i, 1);
-            }
+function deleteToDo(e) {
+    const id = e.target.id;
+    const toDos = getToDos();
+    for (let i = 0; i < toDos.length; i++) {
+        if (id === toDos[i]) {
+            toDos.splice(i, 1);
         }
-        localStorage.removeItem(aufgabeNr);
-        localStorage.setItem('imageGalleryArray', JSON.stringify(imageGalleryArray));
-        ausDOMentfernen(aufgabeNr);
     }
+    localStorage.removeItem(id);
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+    removeFromDOM(id);
 }
 
-function insDOMschreiben(aufgabeNr, ItemObj) {
-    var imageGallery = document.getElementById('imageGallery');
-    var eintrag = document.createElement('img');
-    eintrag.setAttribute('id', aufgabeNr);
-    eintrag.src = ItemObj.value;
-    eintrag.title = "Löschen";
-    imageGallery.appendChild(eintrag);
-    eintrag.onclick = toDoLöschen;
-}
-
-function ausDOMentfernen(aufgabeNr) {
-    var eintrag = document.getElementById(aufgabeNr);
-    eintrag.parentNode.removeChild(eintrag);
-}
-
-function allesLöschen() {
-    localStorage.clear();
-    var ItemList = document.getElementById('imageGallery');
-    var imageGallery = ItemList.childNodes;
-    for (var i = imageGallery.length - 1; i >= 0; i--) {
-        ItemList.removeChild(imageGallery[i]);
-    }
-    var imageGalleryArray = HolEinträge();
+function insertToDOM(id, toDo) {
+    const imageGallery = document.getElementById('imageGallery');
+    const entry = document.createElement('img');
+    entry.setAttribute('id', id);
+    entry.src = toDo.value;
+    entry.title = "Delete";
+    entry.classList.add('animated','fadeIn')  // add an animation
+    imageGallery.appendChild(entry);
+    entry.addEventListener('click', deleteToDo);
 }
